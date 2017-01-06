2a997c13549968e4d531daf76b52ea73d82e0f81
toniau <toni.au94@gmail.com>
2017-01-05 22:33:02 -0500

    fixed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 var app = express();
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 // use ejs and express layouts
 app.set('view engine', 'ejs');
